72cd36577409d90ddf864612694c4fdbc2ce20e4
Audrey Borges <40469654+AudreyKBorges@users.noreply.github.com>
2022-07-13 13:30:33 -0500

    Changes to HTML, JS
    HTML- changed range to reflect user input
    JS- added validation for user input at the prompt and in the guess box

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,37 @@
-let num = Math.floor(Math.random() * 20) + 1;
+while(true) {
+    // Ask user what the maximum number should be
+    let maxNum = prompt("Please enter the maximum number.");
+    let minNum = 1;
+     
+    // Validate user entered number
+    if(!isNaN(maxNum) && maxNum > minNum) {
+        maxVal = Math.floor(maxNum);
+        document.getElementById('max-value').innerHTML = maxVal;
+        break;
+    }
+}
 
 function do_guess() {
     let guess = Number(document.getElementById('guess').value);
     let message = document.getElementById('message');
+    let minNum = 1;
+    let num = Math.floor(Math.random() * maxVal- minNum) + minNum;  
     
-    if(guess == num) {
+    // Validate user guess
+    if(isNaN(guess)) {
+        message.innerHTML = "That is not a number!";
+    }
+    // Check to see if user guess is in range
+    else if ((guess < minNum) || (guess > maxVal)) {
+        message.innerHTML = "That number is not in range, try again.";
+    }
+    else if (guess == num) {
         message.innerHTML = "You got it!";
     }
     else if (guess < num) {
         message.innerHTML = "No, try a higher number.";
     }
-    else {
+    else if (guess > num) {
         message.innerHTML = "No, try a lower number.";
     }
 }
