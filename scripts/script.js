ab25c16b9da6f18cf608ffdb2d5b867ae92c164e
Audrey Borges <40469654+AudreyKBorges@users.noreply.github.com>
2022-07-15 13:37:11 -0500

    Changed Math.floor() to parseInt() and moved else ifs to an else

--- scripts/script.js
+++ scripts/script.js
@@ -5,33 +5,49 @@ while(true) {
      
     // Validate user entered number
     if(!isNaN(maxNum) && maxNum > minNum) {
-        maxVal = Math.floor(maxNum);
+        maxVal = parseInt(maxNum);
         document.getElementById('max-value').innerHTML = maxVal;
         break;
     }
 }
 
+let minNum = 1;
+// generate a random max number
+let num = parseInt(Math.random() * (maxVal - minNum)) + minNum; 
+
 function do_guess() {
+    // user input
     let guess = Number(document.getElementById('guess').value);
     let message = document.getElementById('message');
-    let minNum = 1;
-    let num = Math.floor(Math.random() * maxVal- minNum) + minNum;  
+    // initialize empty array to store input
+    let numList = [];
+    let len = numList.length;
     
-    // Validate user guess
+    // validate user guess
     if(isNaN(guess)) {
         message.innerHTML = "That is not a number!";
     }
-    // Check to see if user guess is in range
+    // check to see if user guess is in range
     else if ((guess < minNum) || (guess > maxVal)) {
         message.innerHTML = "That number is not in range, try again.";
-    }
-    else if (guess == num) {
-        message.innerHTML = "You got it!";
-    }
-    else if (guess < num) {
-        message.innerHTML = "No, try a higher number.";
-    }
-    else if (guess > num) {
-        message.innerHTML = "No, try a lower number.";
+        return;
+    } 
+    else {
+        guess = parseInt(guess);
+        if (guess === num) {
+            numList.push(guess);
+            console.log(guess);
+            message.innerHTML = `You got it! It took you ${len} tries and your guesses were ${numList.join(', ')}`;
+        }
+        else if (guess < num) {
+            numList.push(guess);
+            console.log(guess);
+            message.innerHTML = "No, try a higher number.";
+        }
+        else {
+            numList.push(guess);
+            console.log(guess);
+            message.innerHTML = "No, try a lower number.";
+        }
     }
 }
