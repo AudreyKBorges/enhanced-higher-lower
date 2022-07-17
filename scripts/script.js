d5425cac4c1b4cdc1a1667fccf33c364a1adf3c2
Audrey Borges <40469654+AudreyKBorges@users.noreply.github.com>
2022-07-17 16:07:04 -0500

    Removed onclick in HTML and converted it to arrow function in js, rearranged if statements

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,70 @@
-while(true) {
-    // Ask user what the maximum number should be
-    let maxNum = prompt("Please enter the maximum number.");
+window.onload = function(e){
+
+    // set minimum number and user determined max value
     let minNum = 1;
-     
-    // Validate user entered number
-    if(!isNaN(maxNum) && maxNum > minNum) {
-        maxVal = parseInt(maxNum);
-        document.getElementById('max-value').innerHTML = maxVal;
-        break;
+    let maxVal = "";
+
+    while(true) {
+        // prompt the user to enter a number for the max value
+        let maxNum = prompt("Please enter the maximum number.");
+
+        // validate the user entered number
+        if (!isNaN(maxNum) && maxNum > minNum) {
+            maxVal = parseInt(maxNum);
+            document.getElementById('max-value').innerHTML = maxVal;
+            break;
+        }
     }
-}
 
-let minNum = 1;
-// generate a random max number
-let num = parseInt(Math.random() * (maxVal - minNum)) + minNum; 
+    // generate a random max number
+    const num = Math.ceil(Math.random() * (maxVal - minNum)) + minNum; 
 
-function do_guess() {
-    // user input
+    // guess button
+    const button = document.getElementById('button');
+
+    // initialize an empty array to store user guesses
+    let nums = [];
+
+    // add event listener to button
+    button.addEventListener('click', ()=> {
+
     let guess = Number(document.getElementById('guess').value);
     let message = document.getElementById('message');
-    // initialize empty array to store input
-    let numList = [];
-    let len = numList.length;
     
-    // validate user guess
-    if(isNaN(guess)) {
-        message.innerHTML = "That is not a number!";
-    }
-    // check to see if user guess is in range
-    else if ((guess < minNum) || (guess > maxVal)) {
-        message.innerHTML = "That number is not in range, try again.";
-        return;
+    // prevent duplicate guesses
+    if (nums.includes(guess)) {
+        message.innerHTML = `You have already guessed the number ${guess}`;
     } 
-    else {
-        guess = parseInt(guess);
-        if (guess === num) {
-            numList.push(guess);
-            console.log(guess);
-            message.innerHTML = `You got it! It took you ${len} tries and your guesses were ${numList.join(', ')}`;
-        }
-        else if (guess < num) {
-            numList.push(guess);
-            console.log(guess);
-            message.innerHTML = "No, try a higher number.";
-        }
-        else {
-            numList.push(guess);
-            console.log(guess);
-            message.innerHTML = "No, try a lower number.";
-        }
+    // check if guess is a number
+    else if (isNaN(guess)) {
+        message.innerHTML = 'That is not a number!';
     }
+    // check if guess is in range
+    else if ((guess < minNum) || (guess > maxVal)) {
+        message.innerHTML = 'That number is not in range, try again.';
+    }
+    // display message to user if guess is too high
+    else if (guess > num) {
+        console.log(guess);
+        message.innerHTML = "No, try a lower number.";
+        if (!nums.includes(guess)) {
+            nums.push(guess);
+            }
+    }
+    // display message to user if guess is too low
+    else if (guess < num) {
+        console.log(guess);
+        message.innerHTML = "No, try a higher number.";
+        if (!nums.includes(guess)) {
+            nums.push(guess);
+            }
+    }
+    // display message if user guess is correct
+    else if (guess == num) {
+        console.log(guess);
+        nums.push(guess);
+        message.innerHTML = `You got it! It took you ${nums.length} tries and your guesses were ${nums.join(', ')}`;
+        nums = []
+    }
+});
 }
